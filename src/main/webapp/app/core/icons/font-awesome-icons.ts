3cefe2e419e9f099ce0eaf59a034242afdb0cff6
Alexandru Pop <real.popica@gmail.com>
2021-01-17 12:59:10 +0100

    improve navbar

--- src/main/webapp/app/core/icons/font-awesome-icons.ts
+++ src/main/webapp/app/core/icons/font-awesome-icons.ts
@@ -34,6 +34,9 @@ import {
   faTasks,
   // jhipster-needle-add-icon-import
   faHome,
+  faAddressCard,
+  faUsers,
+  faQuestion,
 } from '@fortawesome/free-solid-svg-icons';
 
 export const fontAwesomeIcons = [
@@ -72,4 +75,7 @@ export const fontAwesomeIcons = [
   faTrashAlt,
   // jhipster-needle-add-icon-import
   faAsterisk,
+  faAddressCard,
+  faUsers,
+  faQuestion,
 ];
